d2c2e810e62e5efd75e27e993653c20987a2ccc6
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-07-01 13:07:51 +0300

    react hw1v1

--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -4,7 +4,6 @@ import s from './statistics.module.css';
 function StatisticsList({ items }) {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>Upload stats</h2>
       <ul className={s.statList}>
         {items.map(item => (
           <Statistics
